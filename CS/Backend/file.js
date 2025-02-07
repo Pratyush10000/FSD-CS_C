0327445ddbf6f7f1943cc8ebab46079c5dec7fe9
Pratyush <800pratyush@gmail.com>
2025-02-07 12:22:39 +0530

    Read&Write

--- CS/Backend/file.js
+++ CS/Backend/file.js
@@ -1,3 +1,21 @@
-const fs=require("fs");
-const data=fs.readFileSync('./data.txt','utf8');
+const fs = require("fs");
+
+const data = fs.readFileSync('./data.txt', 'utf8');
 console.log(data);
+
+if (data.includes("H")) {  // Using includes instead of match
+  console.log("H is present");
+  
+  const newdata = data.replace("H", "AB");  // Ensure correct case
+  
+  fs.writeFileSync("./data.txt", newdata, "utf8");  // Fix encoding
+}
+
+
+//write 
+// fs.writeFileSync("./data1.txt","Hello this is my new work.","utf-8");
+// fs.writeFileSync("./data1.txt","ABES","utf-8"); 
+// fs.appendFileSync("./data1.txt","Engineering college","utf-8");
+
+//rename
+// fs.renameSync("./data2.txt","./data3.txt");
